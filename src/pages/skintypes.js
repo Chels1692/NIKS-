b6b567124502487f96ea63e54cd95b26b626219d
[Sonya Zhen] <[sonyazhen12@gmail.com]>
2023-03-21 16:37:34 -0400

    aboutandskinType

--- src/pages/skintypes.js
+++ src/pages/skintypes.js
@@ -1,6 +1,26 @@
+import './skinType.css';
 const Types = () => {
     return (
-        <div></div>
+        <div>
+            <h1 id="heading">Skin Types</h1>
+            <div id="skinTypes">
+                <h2>Oily Skin</h2>
+                <div className="picDescription">
+                    <img className ="pics" src="/" alt="oilyPic"></img>
+                    <p className="descriptions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero volutpat sed cras ornare arcu. Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Vitae nunc sed velit dignissim. Felis eget velit aliquet sagittis. Enim nunc faucibus a pellentesque. At risus viverra adipiscing at in tellus integer. Volutpat commodo sed egestas egestas fringilla. Donec massa sapien faucibus et molestie. Odio facilisis mauris sit amet massa. Diam vel quam elementum pulvinar etiam non quam lacus. Tempor orci eu lobortis elementum nibh.</p>
+                </div>
+                <h2>Dry Skin</h2>
+                <div className="picDescription">
+                    <img className ="pics" src="/" alt="dryPic"></img>
+                    <p className="descriptions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero volutpat sed cras ornare arcu. Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Vitae nunc sed velit dignissim. Felis eget velit aliquet sagittis. Enim nunc faucibus a pellentesque. At risus viverra adipiscing at in tellus integer. Volutpat commodo sed egestas egestas fringilla. Donec massa sapien faucibus et molestie. Odio facilisis mauris sit amet massa. Diam vel quam elementum pulvinar etiam non quam lacus. Tempor orci eu lobortis elementum nibh.</p>
+                </div>
+                <h2>Combination Skin</h2>
+                <div className="picDescription">
+                    <img className ="pics" src="/" alt="comPic"></img>
+                    <p className="descriptions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero volutpat sed cras ornare arcu. Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Vitae nunc sed velit dignissim. Felis eget velit aliquet sagittis. Enim nunc faucibus a pellentesque. At risus viverra adipiscing at in tellus integer. Volutpat commodo sed egestas egestas fringilla. Donec massa sapien faucibus et molestie. Odio facilisis mauris sit amet massa. Diam vel quam elementum pulvinar etiam non quam lacus. Tempor orci eu lobortis elementum nibh.</p>
+                </div>
+            </div>
+        </div>
     );
 };
 
